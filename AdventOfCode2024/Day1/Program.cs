6374a61aae2df6077957637dd0deb4b57bd0f6fb
Romas <Romas@DESKTOP-05CFLK2>
2024-12-03 01:09:17 +0200

    Day1 & Day2

---
+++ AdventOfCode2024/Day1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list1 = new List<int>();
+            List<int> list2 = new List<int>();
+            string[] rawData = File.ReadAllLines("RawData.txt");
+
+            foreach (string line in rawData)
+            {
+                var items = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                list1.Add(int.Parse(items[0]));
+                list2.Add(int.Parse(items[1]));
+            }
+
+            var order1 = list1.Order().ToArray();
+            var order2 = list2.Order().ToArray();
+
+            var sum = 0;
+            for (int i = 0; i < Math.Min(list1.Count, list2.Count); i++)
+            {
+                sum += Math.Abs(order1[i] - order2[i]);
+            }
+
+            Console.WriteLine(sum);
+            Console.WriteLine();
+
+            var x = order2.CountBy(o => o);
+
+            sum = 0;
+            for (int i = 0; i < list1.Count; i++)
+            {
+                var match = x.FirstOrDefault(kvp => kvp.Key == list1[i]);
+                if (!EqualityComparer<KeyValuePair<int, int>>.Default.Equals(match, default))
+                    sum += list1[i] * match.Value;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ AdventOfCode2024/Day2/Program.cs
@@ -0,0 +1,66 @@
+﻿
+using System.Security.Cryptography;
+
+namespace Day2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int[]> list = new List<int[]>();
+            string[] rawData = File.ReadAllLines("RawData.txt");
+            foreach (string data in rawData)
+            {
+                var items = data.Split(' ');
+                 list.Add(items.Select(x => int.Parse(x)).ToArray());
+            }
+
+            int count = 0;
+            foreach (var item in list)
+            {
+                if (IsSafe(item))
+                    count++;
+            }
+            Console.WriteLine(count);
+            Console.WriteLine();
+        }
+
+        static bool IsSafe(int[] line)
+        {
+            // Determine if the line is strictly increasing or decreasing
+            bool isIncreasing = true, isDecreasing = true;
+
+            for (int i = 1; i < line.Length; i++)
+            {
+                int diff = line[i] - line[i - 1];
+
+                // Check if adjacent difference is within [1, 3]
+                if (Math.Abs(diff) < 1 || Math.Abs(diff) > 3)
+                    return false;
+
+                // Check increasing or decreasing
+                if (diff < 0) isIncreasing = false;
+                if (diff > 0) isDecreasing = false;
+            }
+
+            // Line must be either strictly increasing or strictly decreasing
+            return isIncreasing || isDecreasing;
+        }
+
+        //private static bool IsSafe(int[] item)
+        //{
+        //    bool safe = true;
+        //    var increasing = item[0].CompareTo(item[1]) != 0;
+        //    for (int i = 1; i < item.Count(); i++)
+        //    {
+        //        var diff = Math.Abs(item[i] - item[i + 1]);
+        //        if ((diff < 1 || diff > 3) && (item[i].CompareTo(item[i + 1]) != 0) == increasing)
+        //        {
+        //            safe = false;
+        //            break;
+        //        }
+        //    }
+        //    return safe;
+        //}
+    }
+}
